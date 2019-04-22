8c99ee507ac7c490d64204c32c299842fe9c8ec7
Adam Sadkowski <adam92as@gmail.com>
2019-04-22 20:49:40 +0200

    modifed script.js - zadanie losowanie i logika warunkowa

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-var computerMove, playerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+var computerMove, playerMove, playerMove2, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+}
+if (randomNumber == '2') {
+  playerMove = 'papier';
+} else {
+  playerMove = 'nieznany ruch';
+}
+if (randomNumber == '3') {
+  playerMove2 = 'nożyce';
+} else {
+  playerMove2 = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+printMessage('Mój ruch: ' + playerMove);
+printMessage('Mój ruch: ' + playerMove2);
