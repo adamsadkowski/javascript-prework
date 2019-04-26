984e27a69db3762764008426d7036c877ad260d6
Adam Sadkowski <adam92as@gmail.com>
2019-04-26 22:33:56 +0200

    modifed script.js - ćwiczenie losowanie gracza

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// losowanie komputera
+
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
@@ -23,3 +25,42 @@ if (randomNumber == '3') {
   computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+// losowanie gracza
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '2') {
+  playerMove = 'papier';
+} else {
+  playerMove = 'nieznany ruch';
+}
+if (playerInput == '4') {
+  playerMove = 'kamień';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'nieznany ruch';
+}
+if (playerInput == '5') {
+  playerMove = 'kamień';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
+playerInput = 0;
