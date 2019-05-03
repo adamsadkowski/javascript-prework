7eaeb14f8806df29b7a70cb1e0e383c8b32bd669
Adam Sadkowski <adam92as@gmail.com>
2019-05-03 13:10:34 +0200

    modifed script.js - ćwiczenie - zapisywanie odpowiedzi gracza - correct

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// losowanie komputera
+
 var computerMove, computerMove2, computerMove3, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
@@ -21,3 +23,29 @@ if (randomNumber == '3') {
   computerMove3 = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove3);
+
+// ruch gracza
+
+var playerInput, playerMove, playerMove2, playerMove3;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '2') {
+  playerMove2 = 'papier';
+} else {
+  playerMove2 = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove2);
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '3') {
+  playerMove3 = 'nożyce';
+} else {
+  playerMove3 = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove3);
