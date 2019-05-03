aa0da1c0cdf3bdca39f23956333b9f98ecaed993
Adam Sadkowski <adam92as@gmail.com>
2019-05-03 12:27:30 +0200

    modifed script.js - ćwiczenie logika warunkowa

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-// losowanie komputera
-
-var computerMove, randomNumber;
+var computerMove, computerMove2, computerMove3, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
@@ -9,58 +7,17 @@ if (randomNumber == '1') {
   computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
-randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '2') {
-  computerMove = 'papier';
+  computerMove2 = 'papier';
 } else {
-  computerMove = 'nieznany ruch';
+  computerMove2 = 'nieznany ruch';
 }
-printMessage('Mój ruch: ' + computerMove);
-randomNumber = Math.floor(Math.random() * 3 + 1);
+printMessage('Mój ruch: ' + computerMove2);
 console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '3') {
-  computerMove = 'nożyce';
+  computerMove3 = 'nożyce';
 } else {
-  computerMove = 'nieznany ruch';
+  computerMove3 = 'nieznany ruch';
 }
-printMessage('Mój ruch: ' + computerMove);
-
-// losowanie gracza
-
-var playerMove, playerInput;
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Wpisana odpowiedź to: ' + playerInput);
-if (playerInput == '1') {
-  playerMove = 'kamień';
-} else {
-  playerMove = 'nieznany ruch';
-}
-printMessage('Twój ruch: ' + playerMove);
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Wpisana odpowiedź to: ' + playerInput);
-if (playerInput == '2') {
-  playerMove = 'papier';
-} else {
-  playerMove = 'nieznany ruch';
-}
-if (playerInput == '4') {
-  playerMove = 'kamień';
-} else {
-  playerMove = 'nieznany ruch';
-}
-printMessage('Twój ruch: ' + playerMove);
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Wpisana odpowiedź to: ' + playerInput);
-if (playerInput == '3') {
-  playerMove = 'nożyce';
-} else {
-  playerMove = 'nieznany ruch';
-}
-if (playerInput == '5') {
-  playerMove = 'kamień';
-} else {
-  playerMove = 'nieznany ruch';
-}
-printMessage('Twój ruch: ' + playerMove);
-playerInput = 0;
+printMessage('Mój ruch: ' + computerMove3);
